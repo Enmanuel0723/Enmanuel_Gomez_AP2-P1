f366133b76c009ca2566a52a1be439ea4d117ba8
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2026-05-13 18:31:10 -0400

    Agregando EditScreen

---
+++ app/src/main/java/com/example/enmanuel_gomez_ap2_p1/presentation/entidad/EditScreen.kt
@@ -0,0 +1,31 @@
+package com.example.enmanuel_gomez_ap2_p1.presentation.entidad
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.enmanuel_gomez_ap2_p1.ui.theme.Enmanuel_Gomez_AP2_P1Theme
+
+@Composable
+fun EditScreen(
+    entidadId: Int?,
+    onNavigateBack: () -> Unit
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Edit Screen")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EditScreenPreview() {
+    Enmanuel_Gomez_AP2_P1Theme {
+        EditScreen(entidadId = null, onNavigateBack = {})
+    }
+}
