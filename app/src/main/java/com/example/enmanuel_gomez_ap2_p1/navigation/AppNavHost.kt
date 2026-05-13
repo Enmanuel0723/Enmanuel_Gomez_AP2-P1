368d689f307a4713383ac0c59e04be16ef1c098a
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2026-05-13 18:31:37 -0400

    Agregando navHost configurado

---
+++ app/src/main/java/com/example/enmanuel_gomez_ap2_p1/navigation/AppNavHost.kt
@@ -0,0 +1,42 @@
+package com.example.enmanuel_gomez_ap2_p1.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.example.enmanuel_gomez_ap2_p1.presentation.entidad.EditScreen
+import com.example.enmanuel_gomez_ap2_p1.presentation.entidad.ListScreen
+import kotlinx.serialization.Serializable
+
+@Serializable
+object ListScreenRoute
+
+@Serializable
+data class EditScreenRoute(val entidadId: Int? = null)
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = ListScreenRoute
+    ) {
+        composable<ListScreenRoute> {
+            ListScreen(
+                onNavigateToEdit = { id ->
+                    navController.navigate(EditScreenRoute(entidadId = id))
+                }
+            )
+        }
+        composable<EditScreenRoute> { backStackEntry ->
+            val route = backStackEntry.toRoute<EditScreenRoute>()
+            EditScreen(
+                entidadId = route.entidadId,
+                onNavigateBack = { navController.navigateUp() }
+            )
+        }
+    }
+}
