bdec89c3bce0d887138655a0129e95f3f87c66f1
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2026-05-13 18:29:17 -0400

    Agregando archivo para que se suba carpeta domain/repository

---
+++ app/src/main/java/com/example/enmanuel_gomez_ap2_p1/data/repository/EntidadRepositoryImpl.kt
@@ -0,0 +1,8 @@
+package com.example.enmanuel_gomez_ap2_p1.data.repository
+
+import com.example.enmanuel_gomez_ap2_p1.domain.repository.EntidadRepository
+import javax.inject.Inject
+
+class EntidadRepositoryImpl @Inject constructor() : EntidadRepository {
+
+}
