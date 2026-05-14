58f257a4ff4eff41f01dabfd0493df15f947a4af
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2026-05-13 20:36:26 -0400

    Agregando ChequeEntity y ChequeDao

--- app/src/main/java/com/example/enmanuel_gomez_ap2_p1/data/local/entity/BorrameEntity.kt
+++ app/src/main/java/com/example/enmanuel_gomez_ap2_p1/data/local/entity/BorrameEntity.kt
@@ -3,8 +3,11 @@ package com.example.enmanuel_gomez_ap2_p1.data.local.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Borrame")
-data class BorrameEntity(
-    @PrimaryKey
-    val borrameId: Int? = null
+@Entity(tableName = "Cheques")
+data class ChequeEntity(
+    @PrimaryKey(autoGenerate = true)
+    val chequeId: Int = 0,
+    val numero: Int = 0,
+    val beneficiario: String = "",
+    val monto: Double = 0.0
 )
