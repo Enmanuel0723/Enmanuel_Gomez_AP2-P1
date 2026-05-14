d5db686383a1b989c20798ff212549ed8f2e39d3
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2026-05-13 20:36:07 -0400

    Agregando modelo de dominio Cheque

--- app/src/main/java/com/example/enmanuel_gomez_ap2_p1/domain/model/Entidad.kt
+++ app/src/main/java/com/example/enmanuel_gomez_ap2_p1/domain/model/Entidad.kt
@@ -1,5 +1,8 @@
 package com.example.enmanuel_gomez_ap2_p1.domain.model
 
-data class Entidad(
-    val entidadId: Int? = null
+data class Cheque(
+    val chequeId: Int? = null,
+    val numero: Int = 0,
+    val beneficiario: String = "",
+    val monto: Double = 0.0
 )
