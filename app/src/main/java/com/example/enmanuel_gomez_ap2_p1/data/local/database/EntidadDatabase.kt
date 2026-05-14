97ac807d63aa5897ac52eec14a6c68134f17fdca
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2026-05-13 20:39:13 -0400

    Agregando ChequeDatabase

--- app/src/main/java/com/example/enmanuel_gomez_ap2_p1/data/local/database/EntidadDatabase.kt
+++ app/src/main/java/com/example/enmanuel_gomez_ap2_p1/data/local/database/EntidadDatabase.kt
@@ -2,14 +2,14 @@ package com.example.enmanuel_gomez_ap2_p1.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.enmanuel_gomez_ap2_p1.data.local.dao.EntidadDao
-import com.example.enmanuel_gomez_ap2_p1.data.local.entity.EntidadEntity
+import com.example.enmanuel_gomez_ap2_p1.data.local.dao.ChequeDao
+import com.example.enmanuel_gomez_ap2_p1.data.local.entity.ChequeEntity
 
 @Database(
-    entities = [EntidadEntity::class],
+    entities = [ChequeEntity::class],
     version = 1,
     exportSchema = false
 )
-abstract class EntidadDatabase : RoomDatabase() {
-    abstract fun entidadDao(): EntidadDao
+abstract class ChequeDatabase : RoomDatabase() {
+    abstract fun chequeDao(): ChequeDao
 }
