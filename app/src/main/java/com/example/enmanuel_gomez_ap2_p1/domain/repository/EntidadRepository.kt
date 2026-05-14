68f538753e4e37d7e532b42a153d7329350d6636
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2026-05-13 20:39:22 -0400

    Agregando ChequeRepository e implementacion

--- app/src/main/java/com/example/enmanuel_gomez_ap2_p1/domain/repository/EntidadRepository.kt
+++ app/src/main/java/com/example/enmanuel_gomez_ap2_p1/domain/repository/EntidadRepository.kt
@@ -1,5 +1,11 @@
 package com.example.enmanuel_gomez_ap2_p1.domain.repository
 
-interface EntidadRepository {
+import com.example.enmanuel_gomez_ap2_p1.domain.model.Cheque
+import kotlinx.coroutines.flow.Flow
 
+interface ChequeRepository {
+    fun getCheques(): Flow<List<Cheque>>
+    suspend fun getChequeById(id: Int): Cheque?
+    suspend fun save(cheque: Cheque)
+    suspend fun delete(cheque: Cheque)
 }
